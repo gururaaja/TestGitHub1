8709adfb8700691e5e180a4d45f26721d3f5d3a2
gururaaja <kvb_guru@yahoo.com>
2024-06-14 15:29:53 +0530

    test1
    test1

---
+++ Entities/UserDetails400.cs
@@ -0,0 +1,10 @@
+﻿namespace TestSampleProject1.Entities
+{
+    public class UserDetails
+    {
+        public int SNO { get; set; }
+        public string NAME { get; set; }
+        public string LOCATION { get; set; }
+
+    }
+}
